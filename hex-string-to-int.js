7888abae96cd37d192750f40d83dca03c90d4404
Dave Pitts <dgapitss@gmail.com>
2018-09-30 18:27:03 +0200

    code tidy up for hex-string-to-int.js

--- hex-string-to-int.js
+++ hex-string-to-int.js
@@ -5,12 +5,6 @@
  * @param {str} input string in standard hex format e.g. #cccccc
  * @returns {number} output hex 
  */
-const hexStringToInt = (str) => {
-  return parseInt(str.replace(/#/g, ''),16);
-  // let result = '';
-  // for (var i=0; i<str.length; i++) {
-  //   result += str.charCodeAt(i).toString(16);
-  // }
-};
+const hexStringToInt = (str) => parseInt(str.replace(/#/g, ''),16);
 
 module.exports = hexStringToInt;
